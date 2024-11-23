f54cdb1d9df3c45e16a93fba54665c7a8c92cdc3
chyun <och2510@gmail.com>
2024-11-24 04:45:28 +0900

    [Feat] #13 고민 사용자 해결 api 구현

--- src/main/java/org/sopt/sopkathon/domain/problem/api/service/ProblemService.java
+++ src/main/java/org/sopt/sopkathon/domain/problem/api/service/ProblemService.java
@@ -2,6 +2,7 @@ package org.sopt.sopkathon.domain.problem.api.service;
 
 import org.sopt.sopkathon.domain.problem.api.vo.ProblemVo;
 import org.sopt.sopkathon.domain.problem.api.vo.ProblemsVo;
+import org.sopt.sopkathon.domain.problem.core.Problem;
 import org.sopt.sopkathon.domain.problem.core.ProblemRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,5 +23,21 @@ public class ProblemService {
                         .map(ProblemVo::toProblemVo)
                         .toList()
         );
+    }
+
+    @Transactional
+    public void patchProblem(
+            final long problemId,
+            final long itemId
+    ) {
+        Problem problem = problemRepository.findById(problemId).orElseThrow(
+                () -> new IllegalArgumentException()
+        );
+
+        problem.getItems().forEach(item -> {
+            if (item.getId() == itemId) {
+                item.setIsSelected(true);
+            }
+        });
     }
 }

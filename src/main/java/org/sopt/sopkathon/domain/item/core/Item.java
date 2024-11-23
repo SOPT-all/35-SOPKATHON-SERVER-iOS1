1f85957ef8a1d4bc5dacd3f68bf540b1d1cf815f
chyun <och2510@gmail.com>
2024-11-24 03:39:47 +0900

    [Hotfix] #9 엔티티 매핑 정보 수정

--- src/main/java/org/sopt/sopkathon/domain/item/core/Item.java
+++ src/main/java/org/sopt/sopkathon/domain/item/core/Item.java
@@ -1,19 +1,52 @@
 package org.sopt.sopkathon.domain.item.core;
 
 import jakarta.persistence.*;
+import org.sopt.sopkathon.domain.problem.core.Problem;
 
 @Entity
 @Table(name="item")
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
     private Long id;
 
     private String content;
 
     private Boolean isSelected;
 
-    public Long getId() { return id; }
+    @ManyToOne
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
 
-    public String getContent() { return content; }
+    protected Item() {}
+
+    public Item(
+            final String content,
+            final Problem problem
+    ) {
+        this.content = content;
+        this.isSelected = false;
+        this.problem = problem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Boolean getSelected() {
+        return isSelected;
+    }
+
+    public Problem getProblems() {
+        return problem;
+    }
+
+    public void setProblem(Problem problem) {
+        this.problem = problem;
+    }
 }
